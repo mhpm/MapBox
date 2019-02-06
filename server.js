9939fa451e9ea9aa1eacaa7bbcdc6c582994b985
mhpm <michelleeepm@gmail.com>
2019-02-06 15:55:20 -0600

    fix server

--- server.js
+++ server.js
@@ -1,17 +1,15 @@
 const express = require("express");
-const port = process.env.PORT || 3030;
+const port = process.env.PORT || 8080;
 
 const app = express();
 
 app.use(express.static(__dirname + "/dist/"));
-
 app.get(/.*/, function (req, res) {
   res.sendfile(__dirname + "/dist/index.html");
 });
-
 app.listen(port);
 
-console.log("server started... " + port);
+console.log("server started...");
 
 // const io = require('socket.io')(server);
 
